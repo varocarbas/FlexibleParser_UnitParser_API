fe89566d0f1e41462c7cfd1d56ff20bc75c0acfa
varocarbas <contact@customsolvers.com>
2018-03-26 12:37:32 +0200

    Minor improvements.

--- UnitParserAPI/Services/ParserService.cs
+++ UnitParserAPI/Services/ParserService.cs
@@ -61,11 +61,8 @@ namespace UnitParserAPI.Services
 		{
 			//The methods in this class generate the final strings which are displayed to the user.
 			//All of them follow these rules:
-			//- The names of individual units/prefixes are always shown in lower case.
-			//- The symbols/abbreviations of individual units/prefixes are shown as returned by UnitParser.
 			//- The individual units/prefixes are represented via [name] ([symbol or most common abbreviation]).
 			//- Each unit part is represented by [prefix name (if applicable)][unit name][^exponent (if different than 1)]. 
-			//- Any other string is shown as returned by UnitParser.
 
 			public static string GetIndividualUnitOrPrefix(dynamic unitOrPrefix)
 			{
@@ -102,21 +99,12 @@ namespace UnitParserAPI.Services
 
 			private static string GetUnitOrPrefixName(Units unit)
 			{
-				return GetUnitOrPrefixNameInternal(unit.ToString());
+				return unit.ToString();
 			}
 
 			private static string GetUnitOrPrefixName(Prefix prefix)
 			{
-				return GetUnitOrPrefixNameInternal(prefix.Name.ToString());
-			}
-
-			private static string GetUnitOrPrefixNameInternal(string input)
-			{
-				return 
-				(
-					input == "None" || input == "Unitless" || 
-					input.Contains("Valid") ? input : input.ToLower()
-				);
+				return prefix.Name.ToString();
 			}
 
 			private static string GetUnitOrPrefixSymbol(Units unit)
